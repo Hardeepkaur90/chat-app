c33555a51b7f8ec3225dd350fd31800d143bd1d4
Hardeepkaur90 <simran.tws@tekkiwebsolutions.com>
2022-02-11 09:41:17 +0530

    update code heroku

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = '/chat/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
