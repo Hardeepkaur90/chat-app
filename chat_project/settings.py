85bbf9f6a7fd1c445c1742a76deed05ff7c2420b
Hardeepkaur90 <simran.tws@tekkiwebsolutions.com>
2022-02-11 10:28:44 +0530

    update code heroku

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -119,10 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
-
-LOGOUT_REDIRECT_URL = '/chat/'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
